f41b322609f1d71b97823c213048fe1e0c30a04f
szikaria961 <szikaria961@gmail.com>
2021-02-19 17:01:36 -0500

    Add input states and generate content

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,99 @@
-import React from 'https://cdn.skypack.dev/react';
+import React, { useState } from 'https://cdn.skypack.dev/react';
 import ReactDOM from 'https://cdn.skypack.dev/react-dom';
 
-function Header(props) {
-  return <h1>{props.name}</h1>;
-}
-
 const App = () => {
+  const [bookName, setBookName] = useState("");
+  const [chapterName, setChapterName] = useState("");
+  const [authorName, setAuthorName] = useState("");
+  const [keyIdeas, setKeyIdeas] = useState("");
+  const [mainTakeaway, setMainTakeaway] = useState("");
+  const [questions, setQuestions] = useState("");
+
+  const handleChangeBookName = event => {
+    const { value } = event.target;
+
+    setBookName(value);
+  }
+
+  const handleChangeChapterName = event => {
+    const { value } = event.target;
+
+    setChapterName(value);
+  }
+
+  const handleChangeAuthorName = event => {
+    const { value } = event.target;
+
+    setAuthorName(value);
+  }
+
+  const handleChangeKeyIdeas = event => {
+    const { value } = event.target;
+
+    setKeyIdeas(value);
+  }
+
+  const handleChangeMainTakeaway = event => {
+    const { value } = event.target;
+
+    setMainTakeaway(value);
+  }
+
+  const handleChangeQuestions = event => {
+    const { value } = event.target;
+
+    setQuestions(value);
+  }
+
   return (
     <div>
-      <Header name='Chapter Annotator' />
+      <input
+        onChange={handleChangeBookName}
+        value={bookName}
+        type='text'
+      />
+      <br/>
+      <input
+        onChange={handleChangeChapterName}
+        value={chapterName}
+        type='text'
+      />
+      <br />
+      <input
+        onChange={handleChangeAuthorName}
+        value={authorName}
+        type='text'
+      />
+      <br />
+      <textarea
+        onChange={handleChangeKeyIdeas}
+        value={keyIdeas}
+      >
+      </textarea>
+      <br />
+      <textarea
+        onChange={handleChangeMainTakeaway}
+        value={mainTakeaway}
+      >
+      </textarea>
+      <br />
+      <textarea
+        onChange={handleChangeQuestions}
+        value={questions}
+      >
+      </textarea>
+      <div>
+        Title: {bookName} <br />
+        Chapter Name: {chapterName} <br />
+        Author's Name: {authorName} <br />
+        What are the key ideas? <br />
+          {keyIdeas} <br />
+        Main Takeaways: <br />
+          {mainTakeaway} <br />
+        Questions: <br />
+          {questions} <br />
+
+      </div>
     </div>
   );
 }
